5f5a2685d98b545198fe00b25c7b5250fe85d17f
Ahmed Tammam <tamam65@gmail.com>
2018-03-11 02:14:17 +0200

    Template Design & React Installing

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+
+      </Router>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
